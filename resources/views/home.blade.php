c9a30ab8c04b37ffcf74486c5873e0f85faaaa7b
Robert Joseph Cox Jr <robert@studiorclv.com>
2016-04-08 21:35:46 -0700

    All Main Libraries In Bower, Initial Styleing Starting To Take Place, Introduction Near Complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,21 @@
 @section('content')
 
     <div class="introduction">
-
+        <div class="row fluid">
+            <div class="columns large-6 large-offset-3 medium-8 medium-offset-2 small-12 small-offset-0">
+                <h1 class="aligncenter heading">Studio <span class="highlight">RC</span></h1>
+                <h3 class="aligncenter sub-heading">ROBERT COX - LAS VEGAS, NV - 702.443.3886</h3>
+                <ul class="social menu horizontal">
+                    <li class="icon"><a href="https://www.facebook.com/Psyco430404" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                    <li class="icon"><a href="https://twitter.com/psyco430404" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li class="icon"><a href="https://github.com/Robert430404" target="_blank"><i class="fa fa-github"></i></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 
     <div class="examples">
-
+        <h2 class="aligncenter">Some Of My Work</h2>
     </div>
 
     <div class="about">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,9 @@
 <html>
     <head>
         <title>Studio RC - Web Developement And Design - Robert Joseph Cox Jr. - Las Vegas, NV</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,300,700|Raleway:400,300,700" />
         <link rel="stylesheet" href="{{ asset('/libs/foundation-sites/dist/foundation.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('/libs/font-awesome/css/font-awesome.min.css') }}" />
         <link rel="stylesheet" href="{{ asset('/css/app.css') }}" />
     </head>
 
@@ -11,7 +13,7 @@
             @yield('content')
         </div>
 
-        <script src="{{ asset('/libs/jquery/dist/jqeury.min.js') }}"></script>
+        <script src="{{ asset('/libs/jquery/dist/jquery.min.js') }}"></script>
         <script src="{{ asset('/libs/foundation-sites/dist/foundation.min.js') }}"></script>
         <script src="{{ asset('/js/all.js') }}"></script>
     </body>
