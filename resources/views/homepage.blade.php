14e9ed7ec9a06a9e9daf72af5ad0e418a9939523
Robert Joseph Cox Jr <robert@studiorclv.com>
2016-06-17 22:12:15 -0700

    Updated social and phone number

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,10 +10,10 @@ Modern web design and development for any business big or small. Studiorc specia
         <div class="row fluid">
             <div class="columns large-12 large-offset-0 medium-12 medium-offset-0 small-12 small-offset-0">
                 <h1 class="aligncenter heading">Studio <span class="highlight">RC</span></h1>
-                <h3 class="aligncenter sub-heading">ROBERT COX - LAS VEGAS, NV - 702.443.3886</h3>
+                <h3 class="aligncenter sub-heading">ROBERT COX - LAS VEGAS, NV</h3>
                 <ul class="social menu horizontal">
                     <li class="icon"><a href="https://www.facebook.com/Psyco430404" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                    <li class="icon"><a href="https://twitter.com/psyco430404" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li class="icon"><a href="https://twitter.com/robert430404" target="_blank"><i class="fa fa-twitter"></i></a></li>
                     <li class="icon"><a href="https://github.com/Robert430404" target="_blank"><i class="fa fa-github"></i></a></li>
                 </ul>
             </div>
