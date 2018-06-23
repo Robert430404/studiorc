fa0cb4cc9c377e7bbb0fc896394efd5522836bc7
Robert Joseph Cox Jr <robert@studiorclv.com>
2018-06-22 22:34:03 -0700

    Additional refinements

--- public/index.php
+++ public/index.php
@@ -1,3 +1,33 @@
+<?php
+
+/**
+ * Returns a random hobby from a list.
+ *
+ * @return string
+ */
+function getRandomHobby(): string
+{
+    /**
+     * Just a small list of hobbies that I enjoy.
+     */
+    $hobbies = [
+        'programming',
+        'skate boarding',
+        'playing guitar',
+        'drinking beer',
+        'working',
+    ];
+
+    /**
+     * Random index from the hobby list.
+     */
+    $hobby = array_rand($hobbies);
+
+    return $hobbies[$hobby];
+}
+
+?>
+
 <html>
 <head>
     <title>Studio RC - The online home of Robert Joseph Cox Jr.</title>
@@ -7,53 +37,70 @@
 
     <!-- Load The External Styles -->
     <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
-            integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
-            crossorigin="anonymous"
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+        integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        crossorigin="anonymous"
     />
     <link
-            href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700"
-            rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700"
+        rel="stylesheet"
     />
     <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
     />
 
     <!-- Load The Custom Styles -->
     <link
-            rel="stylesheet"
-            href="./css/index.css"
+        rel="stylesheet"
+        href="./css/index.css"
+    />
+
+    <!-- Register The Favicon -->
+    <link
+        rel="shortcut icon"
+        href="./favicon.ico"
     />
 </head>
 <body>
 <section class="Main__wrapper">
     <header>
-        <img src="./img/headshot.jpeg" alt="Robert Joseph Cox Jr." class="Main__wrapper--headshot"/>
-        <h1>Studio <span class="Main__wrapper--bold">RC</span></h1>
+        <img
+            src="./img/headshot.jpeg"
+            alt="Robert Joseph Cox Jr."
+            class="Main__wrapper--headshot"
+        />
+        <h1>
+            Studio <span class="Main__wrapper--bold">RC</span>
+        </h1>
         <h2>
-            Just a simple dude who enjoys <span class="Main__wrapper--bold">programming</span> <br /> and hanging with the family.
+            Just a simple dude who enjoys
+            <span class="Main__wrapper--bold">
+                <?= getRandomHobby() ?>
+            </span>
+            <br/>
+            and hanging with the family.
         </h2>
         <nav>
             <a
-                    href="https://twitter.com/robert430404"
-                    class="Main__wrapper--externalLink"
-                    target="_blank"
+                href="https://twitter.com/robert430404"
+                class="Main__wrapper--externalLink"
+                target="_blank"
             >
                 <i class="fab fa-twitter"></i>
             </a>
             <a
-                    href="https://github.com/Robert430404"
-                    class="Main__wrapper--externalLink"
-                    target="_blank"
+                href="https://github.com/Robert430404"
+                class="Main__wrapper--externalLink"
+                target="_blank"
             >
                 <i class="fab fa-github"></i>
             </a>
             <a
-                    href="https://layer8.space/@robert"
-                    class="Main__wrapper--externalLink"
-                    target="_blank"
+                href="https://layer8.space/@robert"
+                class="Main__wrapper--externalLink"
+                target="_blank"
             >
                 <i class="fab fa-mastodon"></i>
             </a>
@@ -64,6 +111,9 @@
                 702-931-0864
             </a>
         </nav>
+        <section class="Main__wrapper--copyright">
+            Copyright &copy; <span class="Main__wrapper--bold"><?= date('Y') ?></span>
+        </section>
     </header>
 </section>
 </body>
